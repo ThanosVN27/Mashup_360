6939227b58b66bf590b1794b801e2a9fda04e542
tlle <thanhlongle2704@gmail.com>
2026-05-11 13:55:55 +0200

    fix orca

--- mashup/src/app/components/stock/tabs/reservations/reservations.component.ts
+++ mashup/src/app/components/stock/tabs/reservations/reservations.component.ts
@@ -14,12 +14,13 @@ export class ReservationsComponent {
   readonly sortDate = { sortId: 'pldt', ascending: true };
 
   readonly colonnes: SohoDataGridColumn[] = [
+    // { id: 'type', name: 'Type', field: 'orca', width: 150, align: 'center', filterType: 'text',},
     { id: 'ridn', name: 'N° Commande',    field: 'ridn', width: 200, sortable: true, align: 'center', filterType: 'text' },
     { id: 'ridl', name: 'N° Ligne',       field: 'ridl', width: 200, sortable: true, align: 'center', filterType: 'text' },
     { id: 'agno', name: 'Contrat',        field: 'agno', width: 200, sortable: true, align: 'center', filterType: 'text' },
     { id: 'trqt', name: 'Total réservé',  field: 'trqt', width: 200, sortable: true, align: 'center', filterType: 'decimal', formatter: Soho.Formatters.Integer },
-    { id: 'pldt', name: 'Date planifiée', field: 'pldt', width: 200, sortable: true, align: 'center', filterType: 'text',formatter: (_r: number, _c: number, v: string) => formatM3Date(v),
-    },
+    { id: 'pldt', name: 'Date planifiée', field: 'pldt', width: 200, sortable: true, align: 'center', filterType: 'text',formatter: (_r: number, _c: number, v: string) => formatM3Date(v),},
+    { id: 'stat', name: 'Statut',         field: 'stat', width: 200, align: 'center', filterType: 'text' },
   ];
 
 }
