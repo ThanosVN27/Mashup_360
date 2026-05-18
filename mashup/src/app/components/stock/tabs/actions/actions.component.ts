4cb7f77bbb63b1d48deb41fe3f86e7b74b93d791
tlle <thanhlongle2704@gmail.com>
2026-05-18 16:21:47 +0200

    améliorations

--- mashup/src/app/components/stock/tabs/actions/actions.component.ts
+++ mashup/src/app/components/stock/tabs/actions/actions.component.ts
@@ -48,7 +48,7 @@ export class ActionsComponent implements OnInit, OnChanges {
         formatter: (_r: number, _c: number, v: string) => this.fmtQty(v) },
       { id: 'reservedQuantity',name: 'Qté réservée', field: 'reservedQuantity',width: 120, sortable: true, align: 'center', filterType: 'text',
         formatter: (_r: number, _c: number, v: string) => this.fmtQty(v) },
-      { id: 'differenceQty',   name: 'Différence',   field: 'contractQuantity',width: 120, sortable: true, align: 'center', filterType: 'text',
+      { id: 'differenceQty',   name: 'Reste à commander',   field: 'contractQuantity',width: 150, sortable: true, align: 'center', filterType: 'text',
         formatter: (_r: number, _c: number, _v: string, _col: any, item: any) => this.fmtDiff(item) },
     ];
   }
